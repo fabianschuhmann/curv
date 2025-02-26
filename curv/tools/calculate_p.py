fc01db9f9576f9769abae5c13df9269cab2c4e84
Fabian <fabian@schuhmaenner.net>
2025-02-26 11:44:50 +0100

    found a stupid error

--- curv/tools/calculate_p.py
+++ curv/tools/calculate_p.py
@@ -41,6 +41,7 @@ def fourier_by_layer(layer_group,box_size,Nx=2,Ny=2):
 
 def parallal_frames(u,ndx,box_size,layer_string):
     X,Y=get_XY(box_size)
+    result=[]
     if layer_string.lower()!="Both".lower():
         LayerList=[layer_string]
     else:
@@ -51,25 +52,26 @@ def parallal_frames(u,ndx,box_size,layer_string):
             layer_group_2=u.atoms[[x-1 for x in ndx["Lower"]]]
         else:
             layer_group=u.atoms[[x-1 for x in ndx[Layer]]]
-    if Layer=="Both":
-        Nx, Ny = 2, 2
-        fourier1=fourier_by_layer(layer_group,box_size)
-        fourier2=fourier_by_layer(layer_group_2,box_size)
-        fourier=Fourier_Series_Function(box_size[0], box_size[1], Nx, Ny)
-        fourier.Update_coff(fourier1.getAnm(),fourier2.getAnm()) #For the middle layer, update coefficients
-        Z_fitted_1=np.array([fourier1.Z(xi, yi) for xi, yi in zip(X.flatten(), Y.flatten())]).reshape(X.shape)
-        Z_fitted_2=np.array([fourier2.Z(xi, yi) for xi, yi in zip(X.flatten(), Y.flatten())]).reshape(X.shape)
-        Z_fitted=np.abs(Z_fitted_1-Z_fitted_2)
-    else:
-        fourier=fourier_by_layer(layer_group,box_size)
-        Z_fitted = np.array([fourier.Z(xi, yi) for xi, yi in zip(X.flatten(), Y.flatten())]).reshape(X.shape)
-    curvature = fourier.Curv(X, Y)
-    coordinates = np.vstack([X.flatten(), Y.flatten(), Z_fitted.flatten()]).T
-    Z_fitted=Z_fitted / 10 #Scale from \AA to nm
-    
-    curvature = curvature * 10 #Scale from \AA to nm
+        if Layer=="Both":
+            Nx, Ny = 2, 2
+            fourier1=fourier_by_layer(layer_group,box_size)
+            fourier2=fourier_by_layer(layer_group_2,box_size)
+            fourier=Fourier_Series_Function(box_size[0], box_size[1], Nx, Ny)
+            fourier.Update_coff(fourier1.getAnm(),fourier2.getAnm()) #For the middle layer, update coefficients
+            Z_fitted_1=np.array([fourier1.Z(xi, yi) for xi, yi in zip(X.flatten(), Y.flatten())]).reshape(X.shape)
+            Z_fitted_2=np.array([fourier2.Z(xi, yi) for xi, yi in zip(X.flatten(), Y.flatten())]).reshape(X.shape)
+            Z_fitted=np.abs(Z_fitted_1-Z_fitted_2)
+        else:
+            fourier=fourier_by_layer(layer_group,box_size)
+            Z_fitted = np.array([fourier.Z(xi, yi) for xi, yi in zip(X.flatten(), Y.flatten())]).reshape(X.shape)
+        curvature = fourier.Curv(X, Y)
+        coordinates = np.vstack([X.flatten(), Y.flatten(), Z_fitted.flatten()]).T
+        Z_fitted=Z_fitted / 10 #Scale from \AA to nm
+        
+        curvature = curvature * 10 #Scale from \AA to nm
 
-    return Z_fitted,curvature,Layer
+        result.append((Z_fitted,curvature,Layer))
+    return result
 
 def calc_p(out_dir,u,ndx,From=0,Until=None,Step=1,layer_string="Both",workers=2):
     if Until is None:
@@ -83,6 +85,7 @@ def calc_p(out_dir,u,ndx,From=0,Until=None,Step=1,layer_string="Both",workers=2)
         results=list(executor.map(partial_func,u.trajectory[From:Until:Step]))
     
     #one might consider giving the count list to the parallel and have it save directly, is then hard drive write speed the bottleneck?
-    for count,result in enumerate(results):
-        np.save(f"{out_dir}/Z_fitted_{count+1}_{result[2]}.npy",result[0])
-        np.save(f"{out_dir}/curvature_frame_{count+1}_{result[2]}.npy", result[1])
+    for count,tupels in enumerate(results):
+        for result in tupels:
+            np.save(f"{out_dir}/Z_fitted_{count+1}_{result[2]}.npy",result[0])
+            np.save(f"{out_dir}/curvature_frame_{count+1}_{result[2]}.npy", result[1])
