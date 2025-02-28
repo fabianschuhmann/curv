254495f17f67a1411d8fe4ddd26ba1688f0ef65f
Brian <brian.ratnasinghe@nbi.ku.dk>
2025-02-28 14:04:06 +0100

    Rotation Logic Version 1

--- curv/tools/plot_curvature.py
+++ curv/tools/plot_curvature.py
@@ -20,31 +20,46 @@ def get_XY(box_size):
     X, Y = np.meshgrid(x, y)
     return X,Y
 
-def draw(Dir,layer1="Upper",layer2="Lower",layer3="Both",minmax=None,filename=""):
+def draw(Dir,layer1="Upper",layer2="Lower",layer3="Both",minmax=None,filename="", rotate=False):
     # Plots
     fontsize=24
     box_size=np.load(Dir+"boxsize.npy")
     X,Y = get_XY(box_size)
-    # Upper layer 
+    center_x = X/2
+    center_y = Y/2
+
+    with open('radius_threshold.txt', 'r') as f:
+        radius_threshold = int(float(f.read()))
+
+    distance_from_center = np.sqrt((X - center_x)**2 + (Y - center_y)**2)
+    mask = distance_from_center <= radius_threshold
+
+    # Upper layer
+    prefix = "curvature_frame_" if rotate == False else "curvature_rotation_"
     curvature_data1=[]
-    for file_path in glob.glob(Dir+f"curvature_frame_*_{layer2}.npy"):
+    for file_path in glob.glob(Dir+prefix+f"*_{layer2}.npy"):
         curvature_data1.append(np.load(file_path))
     curvature_data1 = np.asarray(curvature_data1)
     curvature_data1=np.mean(curvature_data1,axis=0)
 
     # Lower layer 
     curvature_data2=[]
-    for file_path in glob.glob(Dir+f"curvature_frame_*_{layer1}.npy"):
+    for file_path in glob.glob(Dir+prefix+f"*_{layer1}.npy"):
         curvature_data2.append(np.load(file_path))
     curvature_data2 = np.asarray(curvature_data2)
     curvature_data2=np.mean(curvature_data2,axis=0)
 
     # Middle layer
     curvature_data3=[]
-    for file_path in glob.glob(Dir+f"curvature_frame_*_{layer3}.npy"):
+    for file_path in glob.glob(Dir+prefix+f"*_{layer3}.npy"):
         curvature_data3.append(np.load(file_path))
     curvature_data3 = np.asarray(curvature_data3)
     curvature_data3=np.mean(curvature_data3,axis=0)
+
+    #if(rotate == True):
+    #    curvature_data1 = np.ma.masked_where(~mask , curvature_data1)
+    #    curvature_data2 = np.ma.masked_where(~mask , curvature_data2)
+    #    curvature_data3 = np.ma.masked_where(~mask , curvature_data3)
 
     catted=[curvature_data1,curvature_data2,curvature_data3]
 
@@ -56,12 +71,18 @@ def draw(Dir,layer1="Upper",layer2="Lower",layer3="Both",minmax=None,filename=""
         bmin, bmax =np.min([np.min(x) for x in catted]),np.max([np.max(x) for x in catted])
         print(f"Automatically, the values would have been assigned to {np.round(bmin,3)} and {np.round(bmax,3)}")
 
+
     # Thickness
+    prefix = "Z_fitted_" if rotate == False else "Z_fitted_rotation_"
     Z_fitted=[]
-    for file_path in glob.glob(Dir+f"Z_fitted_*_{layer3}.npy"):
+    for file_path in glob.glob(Dir+prefix+f"*_{layer3}.npy"):
         Z_fitted.append(np.load(file_path))
     Z_fitted = np.asarray(Z_fitted)
     Z_fitted=np.mean(Z_fitted,axis=0)
+
+    #if(rotate == True):
+    #    curvature_data3 = np.ma.masked_where(~mask , curvature_data3)
+
 
     fig, axes = plt.subplots(2, 2, figsize=(24,28))  # Create 1 row, 2 columns of subplots
     axes=axes.flatten()
@@ -135,6 +156,7 @@ def plot_curvature(args: List[str]) -> None:
     parser.add_argument('--minimum',type=float,default=None,help="Supply a custom colorbar value for the curvature plots (minimum)")
     parser.add_argument('--maximum',type=float,default=None,help="Supply a custom colorbar value for the curvature plots (maximum)")
     parser.add_argument('-o','--outfile',type=str,default="",help="Specify the path to save the image, if none is given, image is shown.")
+    parser.add_argument('-r','--rotation',type=bool,default=False,help="Specify if each frame's fourier transform should be rotated such that each frame's protein has the same orientation")
    
     args = parser.parse_args(args)
     logging.basicConfig(level=logging.INFO)
@@ -145,8 +167,9 @@ def plot_curvature(args: List[str]) -> None:
         minmax=None
 
     try:
-        draw(Dir=args.numpys_directory,layer1=args.layer1,layer2=args.layer2,layer3=args.layer3,minmax=minmax,filename=args.outfile)
+        draw(Dir=args.numpys_directory,layer1=args.layer1,layer2=args.layer2,layer3=args.layer3,minmax=minmax,filename=args.outfile,rotate=args.rotation)
 
     except Exception as e:
         logger.error(f"Error: {e}")
         raise
+
